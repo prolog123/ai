8a06bda76ff23fcb69ee7422748bba534e40f368
prolog123 <132581006+prolog123@users.noreply.github.com>
2023-05-04 21:50:42 +0530

    Update AADirectoryIterator.py

--- AADirectoryIterator.py
+++ AADirectoryIterator.py
@@ -1,5 +1,14 @@
-Create Directory Iterator
+# Neural Network Imp code
 
+import cv2
+import numpy as np
+import pandas as pd
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.utils import img_to_array,to_categorical
+from sklearn.preprocessing import label_binarize
+from sklearn.model_selection import train_test_split
+from keras.layers import Conv2D,Flatten,Dense,MaxPool2D
 
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -44,6 +53,19 @@ ypred=np.argmax(pred,axis=1)
 test_generator.classes
 confusion_matrix(ypred,test_generator.classes)
 
+model=Sequential()
+model.add(Conv2D(32,(3,2),input_shape=(224,224,3),activation='relu'))
+model.add(Conv2D(32,(3,2),activation='relu',kernel_regularizer='l2'))
+model.add(MaxPool2D(pool_size=(3,3)))
+model.add(Conv2D(32,(3,2),activation='relu',kernel_regularizer='l2'))
+model.add(MaxPool2D(pool_size=(3,3)))
+model.add(Conv2D(32,(3,2),activation='relu',kernel_regularizer='l2'))
+model.add(MaxPool2D(pool_size=(3,3)))
+model.add(Flatten())
+model.add(Dense(20,activation='relu'))
+model.add(Dense(3,activation='softmax'))
+model.summary()
+
 model.compile(optimizer=RMSprop(lr=2e-5),loss='categorical_crossentropy',metrics=['accuracy'])
 history = model.fit(train_generator,steps_per_epoch=10,epochs=50,validation_data=validation_generator,validation_steps=5,verbose=2,callbacks=[early_stopping])
 
@@ -56,3 +78,85 @@ plt.xlabel('Number of Epochs')
 plt.ylabel('Loss')
 plt.title('Training Loss v/s Validation Loss')
 plt.legend()
+
+
+
+#convert image to array
+def convertImgToArray(image_path):
+    try:
+        image=cv2.imread(image_path)
+        if(image is not None):
+            image=cv2.resize(image,(256,256))
+            return img_to_array(image)
+        else:
+            return (np.array([]))
+    except Exception as e:
+        print('Error:',e)
+
+path='F:/AI lab/NeuralNetwork/Plant_images'
+root_dir=os.listdir(path)
+root_dir
+
+image_list,image_label=[],[]
+temp=-1
+for dir in root_dir:
+    allImage=os.listdir(path+'/'+dir)
+    temp+=1
+    for file in allImage:
+        newPath=path+'/'+dir+'/'+file
+        image_list.append(convertImgToArray(newPath))
+        image_label.append(temp)
+        
+np.unique(image_label,return_counts=True)
+norm_list=np.array(image_list,dtype=np.float16)/255.0
+ytrain=to_categorical(ytrain)
+ytest=to_categorical(ytest)
+ytrain
+
+from sklearn.metrics import r2_score
+r2_score(ytest,pred)
+
+
+
+# RNN
+
+data.set_index('date')[['Appliances','T_out','RH_out','Visibility']].plot(subplots=True)
+
+TimeseriesGenerator(x,y,length=2,batch_size=1)[0]
+
+xtrain,xtest,ytrain,ytest=train_test_split(x,y,random_state=0,shuffle=False)
+
+win_length=720
+train_generator=TimeseriesGenerator(xtrain,ytrain,length=win_length,batch_size=32)
+test_generator=TimeseriesGenerator(xtest,ytest,length=win_length,batch_size=32)
+
+model=Sequential()
+model.add(LSTM(32,input_shape=(win_length,4),activation='tanh',return_sequences=True))
+model.add(LSTM(32,activation='tanh',return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(32,activation='tanh',return_sequences=False))
+model.add(Dense(1))
+model.summary()
+
+model.compile(loss=keras.losses.MeanSquaredError(),optimizer=keras.optimizers.Adam(),metrics=[keras.metrics.MeanAbsoluteError()])
+
+early_stopping=tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=2,mode='min')
+
+hist=model.fit_generator(train_generator,epochs=1,validation_data=test_generator,shuffle=False,callbacks=[early_stopping])
+
+print(xtest.shape)
+print(pred.shape)
+
+data_pred=pd.concat([pd.DataFrame(pred),pd.DataFrame(xtest[:,1:][win_length:])],axis=1)
+data_pred.shape
+
+inv_data=sca.inverse_transform(data_pred)
+inv_data
+
+data_final=inputs[pred.shape[0]*-1:]
+
+data_final['pred_appli']=inv_data[:,0]
+
+data_final
+
+data_final[['Appliances','pred_appli']].plot()
